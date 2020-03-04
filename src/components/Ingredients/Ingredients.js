ba5555f0ae9f5063b65d189f4279791672f24853
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-03-04 13:35:11 -0500

    deleting ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -53,9 +53,16 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients =>
-      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-    );
+    fetch(
+      `https://react-hooks-b3a98.firebaseio.com/ingredients/${ingredientId}.json`,
+      {
+        method: 'DELETE'
+      }
+    ).then(response => {
+      setUserIngredients(prevIngredients =>
+        prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+      );
+    });
   };
 
   return (
